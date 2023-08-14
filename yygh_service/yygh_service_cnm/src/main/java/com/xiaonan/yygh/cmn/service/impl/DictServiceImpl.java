aba2f74b4d1d12e42350a2c1e65bf91c8af953f5
NJY <850738958@qq.com>
2023-08-14 16:05:52 +0800

    spring Cache+redis实现缓存

--- yygh_service/yygh_service_cnm/src/main/java/com/xiaonan/yygh/cmn/service/impl/DictServiceImpl.java
+++ yygh_service/yygh_service_cnm/src/main/java/com/xiaonan/yygh/cmn/service/impl/DictServiceImpl.java
@@ -10,6 +10,8 @@ import com.xiaonan.yygh.model.cmn.Dict;
 import com.xiaonan.yygh.vo.cmn.DictEeVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -36,6 +38,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
 
 
     //根据数据id查询子数据列表
+    @Cacheable(value = "dict",keyGenerator = "keyGenerator")
     @Override
     public List<Dict> findChlidData(Long id) {
         QueryWrapper<Dict> wrapper = new QueryWrapper<>();
@@ -72,7 +75,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
             e.printStackTrace();
         }
     }
-
+    @CacheEvict(value = "dict", allEntries=true)
     @Override
     public void importDictData(MultipartFile file) {
         try {
